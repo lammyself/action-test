9042c3ce2d79e9508dec69ea812b8a5fe68e962d
darly <1766936395@qq.com>
2023-07-13 00:21:37 +0800

    写入github

--- action_script/lintIssue.js
+++ action_script/lintIssue.js
@@ -1,5 +1,12 @@
+import fs from 'fs'
 const issueText = process.env.ISSUE;
 
 if(issueText.includes('问题描述')) {
-  process.env.ISSUE_CHECK_RESULT = 'pass'
+  fs.writeFile('$GITHUB_ENV', 'ISSUE_CHECK_RESULT=pass', (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    //file written successfully
+  })
 }
