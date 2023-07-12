ca2fdb572cd46f224a202eed0157fe76cb246cac
darly <1766936395@qq.com>
2023-07-13 00:26:31 +0800

    查看环境变量

--- action_script/lintIssue.js
+++ action_script/lintIssue.js
@@ -1,7 +1,9 @@
 import fs from 'fs'
 const issueText = process.env.ISSUE;
+console.log('out')
 
 if(issueText.includes('问题描述')) {
+  console.log('ISSUE_CHECK_RESULT')
   fs.writeFile('$GITHUB_ENV', 'ISSUE_CHECK_RESULT=pass', (err) => {
     if (err) {
       console.error(err)
