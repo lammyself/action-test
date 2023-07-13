c873d15aa1d78ba4f21cbf772a1fb03c48d25342
“darly” <1766936395@qq.com>
2023-07-13 10:19:07 +0800

    设置github变量

--- action_script/lintIssue.js
+++ action_script/lintIssue.js
@@ -1,14 +1,7 @@
-import fs from 'fs'
 const issueText = process.env.ISSUE;
-console.log('out')
 
 if(issueText.includes('问题描述')) {
-  console.log('ISSUE_CHECK_RESULT')
-  fs.writeFile('$GITHUB_ENV', 'ISSUE_CHECK_RESULT=pass', (err) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    //file written successfully
-  })
+  console.log('ISSUE_CHECK_RESULT=pass');
+} else {
+  console.log('ISSUE_CHECK_RESULT=unqualified');
 }
